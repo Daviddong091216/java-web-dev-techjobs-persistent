e2537dade6c77dd547732a2b188bf74e0e93f0a4
David Dong <daviddong201712@gmail.com>
2020-07-01 20:21:31 -0500

    par2 is finished.Persisting Emplorers and Skills. Added skillcontroller.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull(message = "Location is required!")
+    @Size(min = 2, max = 100, message = "Location must be between 2 and 100 characters.")
+    private String location;
 
+    public Employer() {
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
